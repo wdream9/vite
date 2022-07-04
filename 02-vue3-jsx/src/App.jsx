ef52ada99c4e267f5526a3bf91376f468eddd962
王远辉 <3162662415@qq.com>
2022-07-04 22:33:05 +0800

    文件导入 路径别名配置

--- 02-vue3-jsx/src/App.jsx
+++ 02-vue3-jsx/src/App.jsx
@@ -1,8 +1,14 @@
 import { defineComponent } from "vue";
-import './assets/styles/demo.css'
+// import "./assets/styles/demo.css";
+// 引入别名
+import "@styles/demo.css";
+
 // named exports w/ variable declaration: ok
 export const Test = defineComponent({
   render() {
-    return (<div class='demo'>hello vite jsx component</div>);
+    return <div class="demo">
+      hello vite jsx component
+      <span>!!!</span>
+      </div>;
   },
 });
