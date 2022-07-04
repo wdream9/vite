19bf60958f52c8e49a2dc52a2c8ed2984674293e
王远辉 <3162662415@qq.com>
2022-07-04 22:14:25 +0800

    vite中使用css模块以及使用css变量

--- 02-vue3-jsx/src/App.jsx
+++ 02-vue3-jsx/src/App.jsx
@@ -1,8 +1,8 @@
 import { defineComponent } from "vue";
-
+import './assets/styles/demo.css'
 // named exports w/ variable declaration: ok
 export const Test = defineComponent({
   render() {
-    return (<div>hello vite jsx component</div>);
+    return (<div class='demo'>hello vite jsx component</div>);
   },
 });
--- 02-vue3-jsx/src/main.js
+++ 02-vue3-jsx/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import {Test} from './App.jsx'
+// 使用css变量
+
 // import App from './App.vue'
 
 createApp(Test).mount('#app')
