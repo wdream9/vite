7f4580c03706192097c3bb15200267664e555d49
王远辉 <3162662415@qq.com>
2022-07-05 15:50:36 +0800

    vite配置多页面

--- 10-vite-vue-build/vite.config.js
+++ 10-vite-vue-build/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // js构建目标支持的浏览器配置
 import legacy from '@vitejs/plugin-legacy'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,13 @@ export default defineConfig({
     targets:['defaults','not IE 11'] // defaults:默认支持权0.5%的浏览器以及最新的版本;不要再IE11上运行
   })],
   build:{
-    target:'es2015'
-  }
+    target:'es2015',
+    rollupOptions:{
+      input:{
+        main: path.resolve(__dirname,'index.html'),
+        nested: path.resolve(__dirname,'more-pages/page.html')
+      }
+    }
+  },
+
 })
